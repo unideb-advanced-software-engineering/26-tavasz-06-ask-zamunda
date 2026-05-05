cb55a3a64b5aec9790093fe65efe41b2d714e6a4
thor9086 <lacika8190@gmail.com>
2026-05-05 11:44:21 +0200

    fix srs linking

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,7 +29,7 @@ export default defineConfig({
         },
         {
           label: "SRS",
-          link: "/srs.doc",
+          link: "/srs.docx",
         },
         {
           label: "Architekturális karakterisztikák",
